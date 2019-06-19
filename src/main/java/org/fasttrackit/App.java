d2fb7c418b53acb64738ccc6c8512f0471ade945
Alex <adilex99@yahoo.com>
2019-06-19 10:01:34 -0700

    Studdied encapsulation

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -10,12 +10,12 @@ public class App {
         Engine engine = new Engine();
         engine.manufacturer = "Renault";
         Car car = new Car(engine);
-        car.name = "Dacia";
-        car.color = "blue";
+        car.setName("Dacia");
+        car.setColor("blue");
         car.doorCount = 4;
-        car.mileage = 9.5;
-        car.fuelLevel = 50;
-        car.running = true;
+        car.setMileage(9.5);
+        car.setFuelLevel(50);
+        car.setRunning(true);
 
 
 
@@ -24,25 +24,25 @@ public class App {
 
 
         Car car2 = new Car(new Engine());
-        car2.name = "Audi";
-        car2.color = "red";
-        car2.doorCount = 2;
-        car2.mileage = 7.5;
-        car2.fuelLevel = 60;
-        car2.running = false;
+        car.setName("Audi");
+        car.setColor("red");
+        car.doorCount = 4;
+        car.setMileage(6.5);
+        car.setFuelLevel(80);
+        car.setRunning(false);
 
         Car car3 = car;
 
-        System.out.println(car.name);
-        System.out.println(car3.name);
+        System.out.println(car.getName());
+        System.out.println(car3.getName());
 
         System.out.println("Changing values...");
 
-        car.name = "Dacia Logan";
-        System.out.println(car.name);
-        System.out.println(car3.name);
+        car.setName("Dacia Logan");
+        System.out.println(car.getName());
+        System.out.println(car3.getName());
 
-        System.out.println(car.maxSpeed);
+        System.out.println(car.getMaxSpeed());
         System.out.println(car.engine);
 
         double distance = car.accelerate(60, 0.5);
