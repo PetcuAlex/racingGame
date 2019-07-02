acad23b02a4ab912fa9dd41f9e1da58c3e57e352
Alex <adilex99@yahoo.com>
2019-07-02 09:20:41 -0700

    studied polimorphysm

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -7,78 +7,84 @@ package org.fasttrackit;
 public class App {
     public static void main(String[] args) {
 
-        Engine engine = new Engine();
-        engine.manufacturer = "Renault";
-        Car car = new Car(engine);
-        car.setName("Dacia");
-        car.setColor("blue");
-        car.doorCount = 4;
-        car.setMileage(9.5);
-        car.setFuelLevel(50);
-        car.setRunning(true);
+
+        Game game = new Game();
+        game.start();
 
 
-
-        System.out.println("Our engine");
-        System.out.println(car.engine.manufacturer);
-
-
-        Car car2 = new Car(new Engine());
-        car.setName("Audi");
-        car.setColor("red");
-        car.doorCount = 4;
-        car.setMileage(6.5);
-        car.setFuelLevel(80);
-        car.setRunning(false);
-
-        Car car3 = car;
-
-        System.out.println(car.getName());
-        System.out.println(car3.getName());
-
-        System.out.println("Changing values...");
-
-        car.setName("Dacia Logan");
-        System.out.println(car.getName());
-        System.out.println(car3.getName());
-
-        System.out.println(car.getMaxSpeed());
-        System.out.println(car.engine);
-
-        double distance = car.accelerate(60, 0.5);
-        distance = car.accelerate(60, 0.5);
-
-//        demo for static variables ( class variables)
-//        System.out.println("");
-//        System.out.println("Studying static variables");
-//        System.out.println("");
 //
-//        car.totalCount = 1;
-//        System.out.println("Total count from car:" + car.totalCount);
-//        car2.totalCount = 2;
-//        System.out.println("Total count from car after settin car2:" + car.totalCount);
-//        System.out.println("Total count from car2:" + car.totalCount);
-//        System.out.println("Total count from Vehicle class: " + Vehicle.totalCount);
+//        Engine engine = new Engine();
+//        engine.manufacturer = "Renault";
+//        Car car = new Car(engine);
+//        car.setName("Dacia");
+//        car.setColor("blue");
+//        car.doorCount = 4;
+//        car.setMileage(9.5);
+//        car.setFuelLevel(50);
+//        car.setRunning(true);
 //
-
-
-        System.out.println("Total vehicles: " + Vehicle.getTotalCount());
-
-
-        car2.accelerate(100);
-
-
-        System.out.println("Today");
-        System.out.println("");
-        System.out.println(car);
-
-        BatMobile batMobile = new BatMobile();
-        batMobile.setName("Cheating car");
-        batMobile.accelerate(60,1);
-        batMobile.accelerate(60,1);
-
+//
+//
+//        System.out.println("Our engine");
+//        System.out.println(car.engine.manufacturer);
+//
+//
+//        Car car2 = new Car(new Engine());
+//        car.setName("Audi");
+//        car.setColor("red");
+//        car.doorCount = 4;
+//        car.setMileage(6.5);
+//        car.setFuelLevel(80);
+//        car.setRunning(false);
+//
+//        Car car3 = car;
+//
+//        System.out.println(car.getName());
+//        System.out.println(car3.getName());
+//
+//        System.out.println("Changing values...");
+//
+//        car.setName("Dacia Logan");
+//        System.out.println(car.getName());
+//        System.out.println(car3.getName());
+//
+//        System.out.println(car.getMaxSpeed());
+//        System.out.println(car.engine);
+//
+//        double distance = car.accelerate(60, 0.5);
+//        distance = car.accelerate(60, 0.5);
+//
+////        demo for static variables ( class variables)
+////        System.out.println("");
+////        System.out.println("Studying static variables");
+////        System.out.println("");
+////
+////        car.totalCount = 1;
+////        System.out.println("Total count from car:" + car.totalCount);
+////        car2.totalCount = 2;
+////        System.out.println("Total count from car after settin car2:" + car.totalCount);
+////        System.out.println("Total count from car2:" + car.totalCount);
+////        System.out.println("Total count from Vehicle class: " + Vehicle.totalCount);
+////
+//
+//
+//        System.out.println("Total vehicles: " + Vehicle.getTotalCount());
+//
+//
+//        car2.accelerate(100);
+//
+//
+//        System.out.println("Today");
+//        System.out.println("");
+//        System.out.println(car);
+//
+//        BatMobile batMobile = new BatMobile();
+//        batMobile.setName("Cheating car");
+//        batMobile.accelerate(60,1);
+//        batMobile.accelerate(60,1);
 
 
     }
 }
+
 
---
+++ src/main/java/org/fasttrackit/Game.java
@@ -0,0 +1,15 @@
+package org.fasttrackit;
+
+public class Game {
+
+
+    private Vehicle competitor1;
+    private Vehicle getCompetitor2;
+
+    public void start(){
+
+
+    }
+
+    }
+
