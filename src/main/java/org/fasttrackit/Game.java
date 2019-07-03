0308035d2130350a1fb2ff538d88fb5cba8f94d1
Alex <adilex99@yahoo.com>
2019-07-03 10:25:20 -0700

    studied arrays and for loops

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -6,9 +6,36 @@ public class Game {
     private Vehicle competitor1;
     private Vehicle getCompetitor2;
 
-    public void start(){
+    private Track[] tracks = new Track[3];
+
+    public void start() {
+        initializeTracks();
+        displayTrack();
 
 
+    }
+
+    private void displayTrack() {
+        System.out.println("Available tracks: ");
+        for (int i = 0; i < tracks.length; i++) {
+            if (tracks[i] != null) {
+                System.out.println((i + 1) + ". " + tracks[i].getName());
+            }
+        }
+    }
+
+    private void initializeTracks() {
+        Track city = new Track();
+        city.setName("Cluj Track");
+        city.setLength(100);
+
+        tracks[0] = city;
+
+        Track forest = new Track();
+        forest.setName("Faget Track");
+        forest.setLength(20);
+
+        tracks[1] = forest;
     }
 }
 
